395bd043d9fe45c61d9079c0f2039f7ada5b1a30
Multarix <darien2105@gmail.com>
2023-10-28 17:04:40 +1000

    Update the version for userscripts

--- all.user.js
+++ all.user.js
@@ -6,9 +6,9 @@
 // @homepageURL https://github.com/Multarix/Discord-Remove-useless-chat-buttons
 // @supportURL  https://github.com/Multarix/Discord-Remove-useless-chat-buttons/issues
 // @grant       none
-// @version     1.3
+// @version     1.4
 // @author      Multarix
-// @description Working as of: 20/04/2022
+// @description Working as of: 28/10/2022
 // ==/UserScript==
 
 const buttonsToHide = ["Open GIF picker", "Open sticker picker", "Send a gift", "Boost this server"];
